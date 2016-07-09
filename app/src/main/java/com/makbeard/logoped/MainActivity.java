1276c58a9572a5c1057cb1a490b548c6d66e7ab9
Ivan <i.lauhin@gmail.com>
2016-07-09 19:35:09 +0300

    Add audioplayer.

--- app/src/main/java/com/makbeard/logoped/MainActivity.java
+++ app/src/main/java/com/makbeard/logoped/MainActivity.java
@@ -1,12 +1,12 @@
 package com.makbeard.logoped;
 
 import android.content.Intent;
+import android.media.AudioManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
+import android.widget.Button;
 
 import butterknife.OnClick;
 
@@ -14,42 +14,41 @@ import butterknife.OnClick;
  * Стартовая Activity приложения
  * Created 09.07.2016.
  */
-public class MainActivity extends AppCompatActivity implements Const{
+public class MainActivity extends AppCompatActivity {
 
-    private String[] choose;
+    private AudioManager am;
+    private Button play;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        am = (AudioManager) getSystemService(AUDIO_SERVICE);
 
-
-        Spinner spinner = (Spinner)findViewById(R.id.name_spinner);
-
-        ArrayAdapter<?> adapter =
-                ArrayAdapter.createFromResource(this, R.array.child, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        spinner.setAdapter(adapter);
-
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        //test
+        play = (Button) findViewById(R.id.play_button);
+        play.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                choose = getResources().getStringArray(R.array.child);
-            }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-
+            public void onClick(View v) {
+                Log.d("happy", "Player enable");
+                TaleAudioPlayer taleAudioPlayer = new TaleAudioPlayer();
+                taleAudioPlayer.playExm(v.getContext());
             }
         });
+
 
     }
 
     @OnClick(R.id.enter_button)
     protected void onClickEnter() {
         Intent intent = new Intent(this, TaleChooserActivity.class);
-        intent.putExtra(EXTRA_CHOOSE,choose);
         startActivity(intent);
     }
+
+/*    @OnClick(R.id.play_button)
+    protected void play(){
+        Log.d("happy", "Player enable");
+        TaleAudioPlayer taleAudioPlayer = new TaleAudioPlayer();
+        taleAudioPlayer.playExm(this);
+    }*/
 }
