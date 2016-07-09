6bca48c52d5d4d42156d68f82a97a091a6522615
Fenikz <dsmakarov@gmail.com>
2016-07-10 02:23:41 +0300

    Добавлена проверка выбора и правильности картинок

--- app/src/main/java/com/makbeard/logoped/TaleReadPickActivity.java
+++ app/src/main/java/com/makbeard/logoped/TaleReadPickActivity.java
@@ -1,6 +1,5 @@
 package com.makbeard.logoped;
 
-import android.content.Intent;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.net.Uri;
 import android.support.v7.app.AlertDialog;
@@ -11,6 +10,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.makbeard.logoped.db.DbOpenHelper;
 import com.makbeard.logoped.db.tables.TalesTable;
@@ -19,7 +19,8 @@ import com.makbeard.logoped.model.TaleModelSQLiteTypeMapping;
 import com.pushtorefresh.storio.sqlite.impl.DefaultStorIOSQLite;
 import com.pushtorefresh.storio.sqlite.queries.Query;
 
-import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,6 +30,16 @@ public class TaleReadPickActivity extends AppCompatActivity {
     private static final String TAG = TaleReadPickActivity.class.getSimpleName();
     private DefaultStorIOSQLite mDefaultStorIOSQLite;
     private TaleModel mTale;
+
+    private Boolean isImage1Choosen = false;
+    private Boolean isImage2Choosen = false;
+    private Boolean isImage3Choosen = false;
+    private Boolean isImage4Choosen = false;
+
+    private String mChoosenImage1 = "";
+    private String mChoosenImage2 = "";
+    private String mChoosenImage3 = "";
+    private String mChoosenImage4 = "";
 
     @BindView(R.id.text_part1_textview)
     TextView mPart1TextView;
@@ -97,34 +108,37 @@ public class TaleReadPickActivity extends AppCompatActivity {
             ImageView imageView3 = (ImageView) alertDialog.findViewById(R.id.image3);
             ImageView imageView4 = (ImageView) alertDialog.findViewById(R.id.image4);
 
+            int[] partsNumbersArray = { 0, 1, 2, 3 };
+            shuffleArray(partsNumbersArray);
+
             if (imageView1 != null) {
-                imageView1.setImageURI(Uri.parse(mTale.getTaleParts().get(0).getImageLink()));
+                imageView1.setImageURI(Uri.parse(mTale.getTaleParts().get(partsNumbersArray[0]).getImageLink()));
                 imageView1.setOnClickListener(v -> {
-                    setImageToImageView(partNumber, Uri.parse(mTale.getTaleParts().get(0).getImageLink()));
+                    setImageToImageView(partNumber, Uri.parse(mTale.getTaleParts().get(partsNumbersArray[0]).getImageLink()));
                     alertDialog.dismiss();
                 });
             }
 
             if (imageView2 != null) {
-                imageView2.setImageURI(Uri.parse(mTale.getTaleParts().get(1).getImageLink()));
+                imageView2.setImageURI(Uri.parse(mTale.getTaleParts().get(partsNumbersArray[1]).getImageLink()));
                 imageView2.setOnClickListener(v -> {
-                    setImageToImageView(partNumber, Uri.parse(mTale.getTaleParts().get(1).getImageLink()));
+                    setImageToImageView(partNumber, Uri.parse(mTale.getTaleParts().get(partsNumbersArray[1]).getImageLink()));
                     alertDialog.dismiss();
                 });
             }
 
             if (imageView3 != null) {
-                imageView3.setImageURI(Uri.parse(mTale.getTaleParts().get(2).getImageLink()));
+                imageView3.setImageURI(Uri.parse(mTale.getTaleParts().get(partsNumbersArray[2]).getImageLink()));
                 imageView3.setOnClickListener(v -> {
-                    setImageToImageView(partNumber, Uri.parse(mTale.getTaleParts().get(2).getImageLink()));
+                    setImageToImageView(partNumber, Uri.parse(mTale.getTaleParts().get(partsNumbersArray[2]).getImageLink()));
                     alertDialog.dismiss();
                 });
             }
 
             if (imageView4 != null) {
-                imageView4.setImageURI(Uri.parse(mTale.getTaleParts().get(3).getImageLink()));
+                imageView4.setImageURI(Uri.parse(mTale.getTaleParts().get(partsNumbersArray[3]).getImageLink()));
                 imageView4.setOnClickListener(v -> {
-                    setImageToImageView(partNumber, Uri.parse(mTale.getTaleParts().get(3).getImageLink()));
+                    setImageToImageView(partNumber, Uri.parse(mTale.getTaleParts().get(partsNumbersArray[3]).getImageLink()));
                     alertDialog.dismiss();
                 });
             }
@@ -144,16 +158,68 @@ public class TaleReadPickActivity extends AppCompatActivity {
         switch (partNumber) {
             case 0:
                 mImage1ImageView.setImageURI(imageUri);
+                isImage1Choosen = true;
+                mChoosenImage1 = imageUri.toString();
+                isAllImagesPick();
                 break;
             case 1:
                 mImage2ImageView.setImageURI(imageUri);
+                isImage2Choosen = true;
+                mChoosenImage2 = imageUri.toString();
+                isAllImagesPick();
                 break;
             case 2:
                 mImage3ImageView.setImageURI(imageUri);
+                isImage3Choosen = true;
+                mChoosenImage3 = imageUri.toString();
+                isAllImagesPick();
                 break;
             case 3:
                 mImage4ImageView.setImageURI(imageUri);
+                isImage4Choosen = true;
+                mChoosenImage4 = imageUri.toString();
+                isAllImagesPick();
                 break;
+        }
+    }
+
+    /**
+     * Запускается, когда все картинки выбраны
+     */
+    private void isAllImagesPick() {
+        if (isImage1Choosen && isImage2Choosen && isImage3Choosen && isImage4Choosen) {
+            checkCorrectAnswers();
+        }
+    }
+
+    /**
+     * Проверяет правильные ответы, обводит неправильные
+     */
+    private void checkCorrectAnswers() {
+        int i = 0;
+        if (mChoosenImage1.equals(mTale.getTaleParts().get(0).getImageLink())) {
+
+            Log.d(TAG, "checkCorrectAnswers: 1 правильно ");
+            i++;
+        }
+        if (mChoosenImage2.equals(mTale.getTaleParts().get(1).getImageLink())) {
+
+            Log.d(TAG, "checkCorrectAnswers: 2 правильно ");
+            i++;
+        }
+        if (mChoosenImage3.equals(mTale.getTaleParts().get(2).getImageLink())) {
+
+            Log.d(TAG, "checkCorrectAnswers: 3 правильно ");
+            i++;
+        }
+        if (mChoosenImage4.equals(mTale.getTaleParts().get(3).getImageLink())) {
+
+            Log.d(TAG, "checkCorrectAnswers: 4 правильно ");
+            i++;
+        }
+        if (i == 4) {
+            Toast.makeText(this, "Молодец, всё правильно!", Toast.LENGTH_SHORT).show();
+            // TODO: 10.07.2016 Добавить переход в Activity диафильма
         }
     }
 
@@ -175,4 +241,18 @@ public class TaleReadPickActivity extends AppCompatActivity {
                 .executeAsBlocking();
     }
 
+    // Implementing Fisher–Yates shuffle
+    static void shuffleArray(int[] ar)
+    {
+        // If running on Java 6 or older, use `new Random()` on RHS here
+        Random rnd = ThreadLocalRandom.current();
+        for (int i = ar.length - 1; i > 0; i--)
+        {
+            int index = rnd.nextInt(i + 1);
+            // Simple swap
+            int a = ar[index];
+            ar[index] = ar[i];
+            ar[i] = a;
+        }
+    }
 }
